9c8f9967f4da7773ca15f0bfdef6052d617f7b3e
udayshinde259 <udayshinde259@gmail.com>
2026-02-12 22:00:11 +0530

    this last commit of 12-2

--- week-3/promises/medium/mapAsyncLimit.js
+++ week-3/promises/medium/mapAsyncLimit.js
@@ -4,6 +4,17 @@
 // Unlike the standard map, this version should only process a limited number of items concurrently. 
 // As soon as one operation finishes, the next should begin.
 // The final result must preserve the original order of the input array.
-async function mapAsyncLimit(array, limit, asyncFn) {}
+async function mapAsyncLimit(array, limit, asyncFn) {
+    let result = [];
+    for(let i=0;i<array.length;i+=limit){
+        let chunk = array.slice(i,i+limit);
+
+        for(let ele of chunk){
+            let ans = await asyncFn(ele);
+            result.push(ans);
+        }
+    }
+    return result;
+}
 
 module.exports = mapAsyncLimit;
